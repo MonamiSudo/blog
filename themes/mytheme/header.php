d00f185a7a7e8e409995c0e0a1ae7350f26fe71d
須藤百波 <sudomonami@gmail.com>
2022-05-26 02:40:03 +0900

    fv作成

---
+++ themes/mytheme/header.php
@@ -0,0 +1,18 @@
+<?php
+  $nav_args = array(
+    'theme_location' => 'header',
+    'container' => 'nav', // ul（ナビ）を何で囲むか
+    'container_class' => 'l-header__nav', // ulの親のクラス
+    'menu_class' => 'l-header__items', // ulのクラス
+
+  );
+?>
+
+<header class="l-header position-fixed w-100 bg-white">
+  <div class="u-wrapper__1152 l-header-wrapper d-flex align-items-center justify-content-between">
+    <h1 class="mb-0">きぬおブログ</h1>
+    <?php 
+      wp_nav_menu($nav_args);
+    ?>
+  </div>
+</header>
--- themes/mytheme/index.php
+++ themes/mytheme/index.php
@@ -1,12 +1,15 @@
 <!doctype html>
 <html lang="ja">
 <?php include( TEMPLATEPATH . '/components/head.php' ); ?>
-<body>
-  <h1>タイトル</h1>
-  <section>
-    <figure class="mb-0">
-      <img class="u-img__cover" src="<?= $img_path ?>/toppage/fv-salon.jpg" alt="男性が美容院で髪を切っている">
-    </figure>
+<body class="toppage">
+  <?php get_header(); ?>
+  <section class="p-fv pt-20">
+    <div class="position-relative">
+      <figure class="p-fv__img-layout mb-0">
+        <img class="u-img__cover" src="<?= $img_path ?>/toppage/fv-salon.jpg" alt="男性が美容院で髪を切っている">
+      </figure>
+      <h1 class="p-fv__title mb-0 position-absolute text-white lh-base fw-bold ">メンズ美容サイト<br>きぬおブログ</h1>
+    </div>
   </section>
   <?php include( TEMPLATEPATH . '/components/footer.php' ); ?>
 </body>
