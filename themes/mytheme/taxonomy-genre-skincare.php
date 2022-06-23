ee4f3532e4be4c17d86ee4e7edf2e2958e63d862
須藤百波 <sudomonami@gmail.com>
2022-06-23 23:34:16 +0900

    スキンケア用のtaxonomy-{taxonomy}-{term}.phpを作成

--- themes/mytheme/taxonomy-genre-skincare.php
+++ themes/mytheme/taxonomy-genre-skincare.php
@@ -4,7 +4,15 @@
 <body>
   <?php include( TEMPLATEPATH . '/header.php' ); ?>
   <main>
-
+    <section class="pt-20">
+      <h1>taxonomy-genre-skincare.php</h1>
+      <?php if( have_posts() ) : while( have_posts() ) : the_post(); ?>
+        <a href="<?= the_permalink(); ?>">
+          <h2><?php the_title(); ?></h2>
+          <p><?php the_content(); ?></p>
+        </a>
+      <?php endwhile; endif; ?>
+    </section>
   </main>
   <?php include( TEMPLATEPATH . '/components/footer.php' ); ?>
 </body>
